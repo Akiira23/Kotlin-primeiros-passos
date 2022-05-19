5e3de8d7c45aa2354caab0f2ac8e1ba2484de741
Andre Akira <aah950@hotmail.com>
2022-05-19 14:48:32 -0300

    if e/ou when

--- ByteBank/src/main/kotlin/main.kt
+++ ByteBank/src/main/kotlin/main.kt
@@ -6,8 +6,15 @@ fun main() {
     var saldo = 0.0
     saldo += 100
     saldo += 57
+    saldo = -20.0
 
     println("titular: $titular")
     println("numero da conta: $numeroConta")
     println("saldo: $saldo")
+
+    when {
+        saldo > 0 -> println("saldo positivo.")
+        saldo == 0.0 -> println("saldo neutro.")
+        else -> println("saldo negativo.")
+    }
 }
