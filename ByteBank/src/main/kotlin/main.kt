850d1ae65d84c85699968aaecc10394c9af70e5e
Andre Akira <aah950@hotmail.com>
2022-05-19 14:59:18 -0300

    estruturas de repeticao

--- ByteBank/src/main/kotlin/main.kt
+++ ByteBank/src/main/kotlin/main.kt
@@ -1,20 +1,40 @@
 fun main() {
     println("Bem vindo ao ByteBank!")
 
-    val titular = "Alex"
-    val numeroConta = 1000
-    var saldo = 0.0
-    saldo += 100
-    saldo += 57
-    saldo = -20.0
 
-    println("titular: $titular")
-    println("numero da conta: $numeroConta")
-    println("saldo: $saldo")
+    for (i in 7 downTo 1 step 1) {
+        if (i == 6) continue
+        if (i == 3) break
+        val titular = "Alex $i"
+        val numeroConta = 1000 + i
+        var saldo = 100.0 + 10.0 * i
 
+        println("titular: $titular")
+        println("numero da conta: $numeroConta")
+        println("saldo: $saldo")
+        println()
+
+    }
+    var i = 0
+    while (i < 5) {
+        println("while $i")
+        i++
+    }
+//    testaCondicoes(saldo)
+}
+
+private fun testaCondicoes(saldo: Double) {
     when {
         saldo > 0 -> println("saldo positivo.")
         saldo == 0.0 -> println("saldo neutro.")
         else -> println("saldo negativo.")
+    }
+
+    if (saldo > 0) {
+        println("saldo positivo.")
+    } else if (saldo == 0.0) {
+        println("saldo neutro.")
+    } else {
+        println("saldo negativo.")
     }
 }
