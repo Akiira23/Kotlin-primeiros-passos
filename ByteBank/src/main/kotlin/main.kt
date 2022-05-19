78d573ba60b6216b98d023d73e60e540837809df
Andre Akira <aah950@hotmail.com>
2022-05-19 15:42:07 -0300

    conhecendo orientacao objetos

--- ByteBank/src/main/kotlin/main.kt
+++ ByteBank/src/main/kotlin/main.kt
@@ -1,7 +1,55 @@
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
 fun main() {
     println("Bem vindo ao ByteBank!")
 
+    testaCopiasEReferencias()
 
+}
+
+private fun testaCopiasEReferencias() {
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 100
+    contaAlex.saldo = 100.00
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 101
+    contaFran.saldo = 500.00
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println("depositando na conta do Alex")
+    deposita(contaAlex, 50.0)
+    println(contaAlex.saldo)
+    println("depositando na conta do Fran")
+    deposita(contaFran, 75.0)
+    println(contaFran.saldo)
+
+//    val contaJoao = Conta()
+//    contaJoao.titular = "Joao"
+//    var contaMaria = contaJoao
+//    contaMaria.titular = "Maria"
+//
+//    println("titular conta joao: ${contaJoao.titular}")
+//    println("titular conta maria: ${contaMaria.titular}")
+}
+
+fun deposita(conta: Conta, valor: Double) {
+    conta.saldo += valor
+}
+
+private fun testaRepeticao() {
     for (i in 7 downTo 1 step 1) {
         if (i == 6) continue
         if (i == 3) break
@@ -20,7 +68,6 @@ fun main() {
         println("while $i")
         i++
     }
-//    testaCondicoes(saldo)
 }
 
 private fun testaCondicoes(saldo: Double) {
