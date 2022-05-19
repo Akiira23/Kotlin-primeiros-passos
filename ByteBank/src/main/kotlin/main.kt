057a00237e3821f839fe8a686b8c9479dcfd4556
Andre Akira <aah950@hotmail.com>
2022-05-19 14:41:12 -0300

    Criacao do app

---
+++ ByteBank/src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Bem vindo ao ByteBank!")
+
+    val titular = "Alex"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo += 100
+    saldo += 57
+
+    println("titular: $titular")
+    println("numero da conta: $numeroConta")
+    println("saldo: $saldo")
+}
