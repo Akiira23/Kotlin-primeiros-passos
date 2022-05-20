1ddb91a1677321b72d1e4c8de25d616c514b4122
Andre Akira <aah950@hotmail.com>
2022-05-20 08:35:42 -0300

    criando novas classes

--- ByteBank/src/main/kotlin/main.kt
+++ ByteBank/src/main/kotlin/main.kt
@@ -1,6 +1,32 @@
 fun main() {
     println("Bem vindo ao ByteBank!")
-    testaCopias()
+    println()
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "444.111.222-33",
+        salario = 1500.00
+    )
+    println("nome: ${alex.nome}")
+    println("cpf: ${alex.cpf}")
+    println("salario: ${alex.salario}")
+    println("bonificacao: ${alex.bonificacao()}")
+    println()
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "555.222.232-55",
+        salario = 2500.00,
+        senha = 1234
+    )
+    println("nome: ${fran.nome}")
+    println("cpf: ${fran.cpf}")
+    println("salario: ${fran.salario}")
+    println("bonificacao: ${fran.bonificacao()}")
+    if (fran.autentica(1234)) {
+        println("autenticado")
+    } else {
+        println("nao autenticado")
+    }
 }
 
 
