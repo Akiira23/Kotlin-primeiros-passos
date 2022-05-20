fa39e0b3471df7b5d0ff9d562a3bf933da668bfe
Andre Akira <aah950@hotmail.com>
2022-05-20 15:40:13 -0300

    sobrescrita do toString()

--- ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -5,13 +5,15 @@ fun main() {
     println()
 
 
-    val endereco = Endereco()
-    imprime(Any())
-    imprime(1)
-    imprime(1.0)
-    imprime(endereco)
+    val endereco = Endereco(logradouro = "Rua Vergueiro", numero = 715, complemento = "apto 312", estado = "Sao Paulo")
+    val enderecoNovo = Endereco(logradouro = "Av. Paulista", numero = 2012, cidade = "Sao Paulo")
+    println(endereco.equals(endereco))
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+    println(endereco.toString())
+    println(enderecoNovo.toString())
 }
 
 fun imprime(valor: Any) {
-    println(valor)
+
 }
