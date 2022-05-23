86778d71783adea164bcc9486a1114eedd16a7fc
Andre Akira <aah950@hotmail.com>
2022-05-23 15:34:55 -0300

    conhecendo a base de collection

--- ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,4 +1,33 @@
 fun main() {
+    val banco = BancoDeNomes()
+    val nomesSalvos = banco.nomes
+    banco.salva("Alex")
+    println(nomesSalvos)
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)  //companion object manter os dados
+}
 
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = _nomes.toList()
+
+    fun salva(nome: String) {
+        _nomes.add(nome)
+    }
+
+    companion object {
+        private val _nomes = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf("Alex", "Fran", "Gui", "Maria", "Ana")
+    for (nome in nomes) {
+        println(nome)
+    }
+//    nomes.add("Paulo")
+//    nomes.remove("Alex")
+    println("contem o Alex: ${nomes.contains("Alex")}")
+    println("tamanho da colecao: ${nomes.size}")
 }
 
