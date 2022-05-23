36b63f836b823a6f27bcfb5693f049d65a78034b
Andre Akira <aah950@hotmail.com>
2022-05-23 08:45:35 -0300

    conhecendo pilhas de execucao e tratando exceptions

--- ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,7 +1,57 @@
+import br.com.alura.bytebank.modelo.Endereco
+
 fun main() {
-    println("Bem vindo ao ByteBank!")
-    println()
+    funcaoMain()
 }
 
+private fun funcaoMain() {
+    println("início main")
 
+    val entrada: String = "1,9"
+    val valor: Double? = try {
+        entrada.toDouble()
+    } catch (e: java.lang.NumberFormatException) {
+        println("Problema na conversao")
+        e.printStackTrace()
+        null
+    }
 
+    val valorComTaxa: Double? = if (valor != null){
+        valor + 0.3
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor invalido")
+    }
+
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1() {
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    try {
+        for (i in 1..5) {
+            println(i)
+            val endereco = Any()
+            endereco as Endereco
+        }
+    } catch (e: java.lang.ClassCastException) {
+//        println(e.message)
+//        println(e.stackTrace)
+//        println(e.cause)
+        e.printStackTrace()
+        println("Class exception")
+    }
+    println("fim funcao2")
+}
