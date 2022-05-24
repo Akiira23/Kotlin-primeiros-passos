b7740ea839c82cdbf7576f2bab357840ba2b889f
Andre Akira <aah950@hotmail.com>
2022-05-24 08:50:20 -0300

    criando colecoes com set

--- ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,33 +1,38 @@
 fun main() {
-    val banco = BancoDeNomes()
-    val nomesSalvos = banco.nomes
-    banco.salva("Alex")
-    println(nomesSalvos)
-    println(banco.nomes)
-    println(BancoDeNomes().nomes)  //companion object manter os dados
+//    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
+//    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
+//    //val assistiramAmbos = mutableListOf<String>()
+//    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
+//    //assistiramAmbos.addAll(assistiramCursoAndroid)
+//    println(assistiramAmbos)
+//    //assistiramAmbos.addAll(assistiramCursoKotlin)
+//    println(assistiramAmbos.distinct())
+
+    val assistiramSetCursoAndroid: MutableSet<String> = mutableSetOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramSetCursoKotlin: MutableSet<String> = mutableSetOf("Alex", "Paulo", "Maria")
+    println("android: $assistiramSetCursoAndroid")
+    println("kotlin: $assistiramSetCursoKotlin")
+//    val assistiramSetAmbos = assistiramSetCursoAndroid + assistiramSetCursoKotlin
+    val assistiramSetAmbos = mutableSetOf<String>()
+    assistiramSetAmbos.addAll(assistiramSetCursoAndroid)
+    assistiramSetAmbos.addAll(assistiramSetCursoKotlin)
+    assistiramSetAmbos.add("Ana")
+    println(assistiramSetAmbos)
+
+    println(assistiramSetCursoAndroid + assistiramSetCursoKotlin)       // une qualuqer tipo de collections
+    println(assistiramSetCursoAndroid union assistiramSetCursoKotlin)  //union une apenas set's
+
+    println(assistiramSetCursoKotlin - assistiramSetCursoAndroid)
+    println(assistiramSetCursoAndroid subtract  assistiramSetCursoKotlin)
+
+    println(assistiramSetCursoAndroid intersect assistiramSetCursoKotlin)
+
+    val assistiramList = assistiramSetAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
+
 }
 
-class BancoDeNomes {
 
-    val nomes: Collection<String> get() = _nomes.toList()
-
-    fun salva(nome: String) {
-        _nomes.add(nome)
-    }
-
-    companion object {
-        private val _nomes = mutableListOf<String>()
-    }
-}
-
-fun testaColecao() {
-    val nomes: Collection<String> = mutableListOf("Alex", "Fran", "Gui", "Maria", "Ana")
-    for (nome in nomes) {
-        println(nome)
-    }
-//    nomes.add("Paulo")
-//    nomes.remove("Alex")
-    println("contem o Alex: ${nomes.contains("Alex")}")
-    println("tamanho da colecao: ${nomes.size}")
-}
 
