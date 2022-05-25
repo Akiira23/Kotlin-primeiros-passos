92c2ac0f1669a888789bb242838f473f3f9c3d06
Andre Akira <aah950@hotmail.com>
2022-05-25 08:52:51 -0300

    utilizando tipo funcao

--- ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ ByteBank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,8 +1,39 @@
-import br.com.alura.bytebank.collection.testaAssociacao
-
 fun main() {
-    testaAssociacao()
+    testaTipoFuncaoReferencia()
+    testaTipoFuncaoClasse()
+    val minhaFuncaoLambda = {          //: () -> Unit
+        println("executa como o lambda")
+    }
+    println(minhaFuncaoLambda())
+    val minhaFuncaoAnonima: () -> Unit = fun() {          //: () -> Unit
+        println("executa como anonima")
+    }
+    println(minhaFuncaoAnonima())
 }
+
+fun testaTipoFuncaoClasse() {
+    val minhaFuncaoClass =
+        Teste()  //funciona por causa do invoke, nao necessariamente precisa colocar o tipo funcao (: () -> Unit)
+    println(minhaFuncaoClass())
+}
+
+fun testaTipoFuncaoReferencia() {
+    val minhaFuncao: () -> Unit = ::teste    // funcao -> tipo de retorno = ::referencia da funcao
+    println(minhaFuncao())
+}
+
+fun teste() {
+    println("executa teste")
+}
+
+class Teste : () -> Unit {
+    override fun invoke() {
+        println("executa invoke do teste")
+    }
+
+}
+
+
 
 
 
